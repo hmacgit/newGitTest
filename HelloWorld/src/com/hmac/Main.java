36a6e367f1fc385ffaafbecef3b98254c6ec8f9d
Hung Mac <hmac.git@gmail.com>
2015-11-08 16:39:23 -0500

    rmi feature F2 number 4

--- HelloWorld/src/com/hmac/Main.java
+++ HelloWorld/src/com/hmac/Main.java
@@ -10,5 +10,13 @@ public class Main {
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
+
+        //rmi feature F2 number 4
+        System.out.println("countdown");
+        for (int i = 5; i > 0; i--)
+            System.out.println(i);
+        System.out.println("LAUNCH");
+
+
     }
 }
