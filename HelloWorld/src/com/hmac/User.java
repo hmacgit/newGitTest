1f5461fe7879dd06e7ac8956e730b6eec47c36a8
Hung Mac <hmac.git@gmail.com>
2015-11-15 14:19:51 -0500

    Added User.java F3

---
+++ HelloWorld/src/com/hmac/User.java
@@ -0,0 +1,71 @@
+package com.hmac;
+
+/**
+ * Created by hungmac on 2015-11-15.
+ */
+//Feature 3-6
+public class User {
+
+    String firstName;
+    String lastName;
+    int age;
+    String email;
+    String address;
+    String phoneNumber;
+
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+}
